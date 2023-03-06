60fdbe0154489cfda733ffac2d2fab4b2dd385dc
OpsDev <ops2dev@hotmail.com>
2023-03-06 13:51:31 +0700

    First Initial

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "SUB_ID" {
+  type = string
+  default = "rsg-test-tf"
+}
+
+variable "TENANT_ID" {
+  type = string
+  default = "rsg-test-tf"
+}
+
+variable "RG_NAME" {
+  type = string
+  default = "rsg-test-tf"
+}
+
+variable "LOCATION_NAME" {
+  type = string
+  default = "southeastasia"
+}
+
+variable "ACR_NAME" {
+  type = string
+  default = "ops2dev"
+}
+
+variable "ASP_NAME" {
+  type = string
+  default = "asp-test-tf"
+}
+
+variable "OS_TYPE" {
+  type = string
+  default = "Linux"
+}
+
+variable "SKU_NAME" {
+  type = string
+  default = "P1v3"
+}
+
+variable "WEB_NAME" {
+  type = string
+  default = "P1v3"
+}
+
+variable "IMG_NAME" {
+  type = string
+  default = "P1v3"
+}
